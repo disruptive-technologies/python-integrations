c0aae8fca5a67937cf8bb8be4bb7351c2d11e851
Johannes H. Gjeraker <jhgjeraker@protonmail.com>
2022-01-12 10:49:52 +0100

    tests updated to reflect metadata changes

--- tests/events.py
+++ tests/events.py
@@ -16,9 +16,9 @@ class Event():
 
 touch = Event(
     headers={
-        'x-dt-signature': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVja3N1bSI6ImE4MTA3ODRkMTNkMThkN2M5ZDU5MzdkNDZlYWM4Nzc0OTZhMTQxOTMiLCJleHAiOjE2MjI3MjI5MjksImlhdCI6MTYyMjcxOTMyOSwianRpIjoiYzJzYm1vOTU2ODNpaWt1dThzdmcifQ.wAhA56Z9YovhvHHU017qDLrcHT2IjzTwTPQL-b3VIlc'  # noqa
+        'X-Dt-Signature': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVja3N1bSI6IjVlM2ExZGRiNTE2ZTU1ZTdlMjc2MWYxNmE3ZWI2OGY3MWMwNmE4MmQiLCJleHAiOjE2NDE5ODQwNzIsImlhdCI6MTY0MTk4MDQ3MiwianRpIjoiYzdmYTRlNzdnZjg3ZWlmNmozdGcifQ.sAGby82i5uWE58Ce8QgeNZt50nvZoxb1wEjBJUBNEIM'  # noqa
     },
-    body_str='{"event":{"eventId":"c2sbmoahed3n71nk69sg","targetName":"projects/c14u9p094l47cdv1o3pg/devices/emuc17m6d7lq0bgk44smcqg","eventType":"touch","data":{"touch":{"updateTime":"2021-06-03T11:22:09.431599Z"}},"timestamp":"2021-06-03T11:22:09.431599Z"},"labels":{"name":"touch"}}',  # noqa
-    body={'event': {'eventId': 'c2sbmoahed3n71nk69sg', 'targetName': 'projects/c14u9p094l47cdv1o3pg/devices/emuc17m6d7lq0bgk44smcqg', 'eventType': 'touch', 'data': {'touch': {'updateTime': '2021-06-03T11:22:09.431599Z'}}, 'timestamp': '2021-06-03T11:22:09.431599Z'}, 'labels': {'name': 'touch'}},  # noqa
-    payload={'checksum': 'a810784d13d18d7c9d5937d46eac877496a14193', 'exp': 1622722929, 'iat': 1622719329, 'jti': 'c2sbmo95683iikuu8svg'},  # noqa
+    body_str='{"event":{"eventId":"c7fa4e34fet12rl646l0","targetName":"projects/c14u9p094l47cdv1o3pg/devices/emuc17m6d7lq0bgk44smcqg","eventType":"touch","data":{"touch":{"updateTime":"2022-01-12T09:41:12.108482Z"}},"timestamp":"2022-01-12T09:41:12.108482Z"},"labels":{"name":"touch"},"metadata":{"deviceId":"emuc17m6d7lq0bgk44smcqg","projectId":"c14u9p094l47cdv1o3pg","deviceType":"touch","productNumber":""}}',  # noqa
+    body={'event': {'eventId': 'c7fa4e34fet12rl646l0', 'targetName': 'projects/c14u9p094l47cdv1o3pg/devices/emuc17m6d7lq0bgk44smcqg', 'eventType': 'touch', 'data': {'touch': {'updateTime': '2022-01-12T09:41:12.108482Z'}}, 'timestamp': '2022-01-12T09:41:12.108482Z'}, 'labels': {'name': 'touch'}, 'metadata': {'deviceId': 'emuc17m6d7lq0bgk44smcqg', 'projectId': 'c14u9p094l47cdv1o3pg', 'deviceType': 'touch', 'productNumber': ''}},  # noqa
+    payload={'checksum': '5e3a1ddb516e55e7e2761f16a7eb68f71c06a82d', 'exp': 1641984072, 'iat': 1641980472, 'jti': 'c7fa4e77gf87eif6j3tg'},  # noqa
 )
